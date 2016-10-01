e1cdfbb26a52bdbdfe1fabc950f1b40daa0c39f1
robsonbiank <robsonbiank@hotmail.com>
2016-10-01 00:05:26 -0300

    menuzinho

---
+++ Menu_Cliente/src/Launch.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Launch {
+	
+	public static void main(String [] args){
+		int icliente = 0;
+		String scliente = null;
+		
+		Scanner entrada =  new Scanner(System.in);
+		
+		System.out.println("Escolha uma das opções" );
+		System.out.println("1 - Incluir | 2 - Alterar | 3 - Excluir | 4 - Listar | 5 - Buscar " );
+		
+		
+		
+		
+	}
+
+}
