a5ece2275c5b86ef5d5eebfa23035d9e90d4017e
robsonbiank <robsonbiank@hotmail.com>
2016-10-01 00:53:50 -0300

    segundoPassoDasListas

--- ListasDePoo/src/Lista001/ContaCorrente.java
+++ ListasDePoo/src/Lista001/ContaCorrente.java
@@ -1,5 +1,39 @@
 package Lista001;
 
 public class ContaCorrente {
+	
+		int numeroConta;
+		String correntista;
+		Float saldo;
+		
+		public ContaCorrente(int numeroConta, String correntista){
+			this.numeroConta = numeroConta;
+			this.correntista = correntista;
+			
+			if(correntista != null ){
+				this.correntista = correntista;
+			}else{
+				System.out.println("Campo não pode ser nulo.");
+			}
+			
+			if(saldo > 0){
+				this.saldo=saldo;
+			}else{
+				System.out.println("Saldo não pode ser menor que zero.");
+			}
+			
+		}
+		
+		public int getNumeroConta(){
+			return numeroConta;
+		}
+		
+		public void setNumeroConta(int NumeroConta){
+			if(numeroConta >= 0 ){
+				this.numeroConta = numeroConta;
+			}else{
+				System.out.println("Número da Conta não pode ser menor que zero.");
+			}
+		}
 
 }
--- ListasDePoo/src/Lista001/Principal.java
+++ ListasDePoo/src/Lista001/Principal.java
@@ -4,6 +4,9 @@ public class Principal {
 	
 	public static void main(String[] args){
 		
+		ContaCorrente c1 = new ContaCorrente(50000, "Robson Biank");
+		ContaCorrente c2 = new ContaCorrente(-10, "Biank");
+		
 	}
 
 }
